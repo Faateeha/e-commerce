e60ff78f357dfb17c04ebd31dba1ab55ba35f34a
Adeyemi Fathia <fathiaomolara02@gmail.com>
2025-04-24 06:12:04 +0100

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,147 @@
+/* eslint-disable no-unused-vars */
+import baby1 from './baby1.jpg'
+import baby2 from './baby2.jpg'
+import capfull from './capfull.jpg'
+import kaftan1 from './kaftan1.jpg'
+import kaftan2 from './kaftan2.jpg'
+import kaftan3 from './kaftan3.jpg'
+import kaftan4 from './kaftan4.jpg'
+import kaftan5 from './kaftan5.jpg'
+import kaftan6 from './kaftan6.jpg'
+import kaftan7 from './kaftan7.jpg'
+import kaftan8 from './kaftan8.jpg'
+import kaftan9 from './kaftan9.jpg'
+import kaftanfull from './kaftanfull.jpg'
+import kimar1 from './kimar1.jpg'
+import kimar2 from './kimar2.jpg'
+import kimar3 from './kimar3.jpg'
+import kimarfull from './kimarfull.jpg'
+import mablogo from './mablogo.jpg'
+
+
+export const assets = {
+    mablogo,
+    kaftanfull
+}
+export const products = [
+    {
+        _id: "1",
+        name: "Black floral dress",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [baby1],
+        category: "baby",
+        subcategory: "baby",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "2",
+        name: "White floral dress",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [baby2],
+        category: "baby",
+        subcategory: "baby",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "3",
+        name: "Inner Cap",
+        description: "A lightweight, beautiful confy Inner cap",
+        price: 600,
+        image: [capfull],
+        category: "cap",
+        subcategory: "capp",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "4",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan1],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "5",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan2],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "6",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan3],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "7",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan4],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "8",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan5],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: true
+    },
+    {
+        _id: "9",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan6],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: false
+    },
+    {
+        _id: "10",
+        name: "Senator material",
+        description: "A lightweight, beautiful confy baby dress",
+        price: 20000,
+        image: [kaftan7],
+        category: "Male",
+        subcategory: "German wool",
+        sizes: ["S", "M", "L"],
+        date: 20042025,
+        bestseller: false
+    },
+]
